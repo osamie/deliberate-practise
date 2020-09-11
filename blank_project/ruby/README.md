d461ff3d160ba9c7fb8f9b09ebc8a01cc10456d5
Zuwa Omigie <zuwa.omigie@shopify.com>
2020-09-11 08:46:01 -0400

    blank project

---
+++ blank_project/ruby/README.md
@@ -0,0 +1,13 @@
+This a blank ruby project ready to run with MiniTest
+
+To set up this project, run:
+
+```
+dev up
+```
+
+To run the tests:
+
+```
+dev test
+```
---
+++ blank_project/ruby/test/project_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+require 'project'
+
+class ProjectTest < Minitest::Test
+  def test_addition
+    assert_equal true, false, 'Good this failed, tests are executing correctly'
+  end
+end
